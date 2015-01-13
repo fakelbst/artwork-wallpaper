bdd729b9689e1b718dc1b14454cf72634e43a342
mao <myj226@gmail.com>
2015-01-13 21:35:13 +0800

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+
+var uglify = require('gulp-uglify');
+
+var paths = {
+  jspath: ['web/static/js/**/*.js', '!web/static/js/common/libs/**/*.js']
+};
+
+gulp.task('default', []);
