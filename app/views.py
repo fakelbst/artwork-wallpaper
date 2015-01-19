04fee54c3d281221365838386ae313424f8a0507
mao <myj226@gmail.com>
2015-01-20 01:22:03 +0800

    test celery

--- app/views.py
+++ app/views.py
@@ -1,10 +1,34 @@
 import urllib, cStringIO, json, random
 import PIL
 import requests
-from flask import render_template, send_file
+from flask import render_template, send_file, jsonify
 from StringIO import StringIO
 from app import app
 from PIL import Image
+from celery import Celery
+
+app.config.update(
+    CELERY_BROKER_URL='redis://localhost:6379',
+    CELERY_RESULT_BACKEND='redis://localhost:6379'
+)
+
+def make_celery(app):
+    celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
+    celery.conf.update(app.config)
+    TaskBase = celery.Task
+    class ContextTask(TaskBase):
+        abstract = True
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return TaskBase.__call__(self, *args, **kwargs)
+    celery.Task = ContextTask
+    return celery
+
+celery = make_celery(app)
+
+@celery.task()
+def add_together(a, b):
+    return a + b
 
 @app.route('/')
 def index():
@@ -20,40 +44,11 @@ def test():
 
     return send_file(img_io, mimetype='image/png')
 
-@app.route('/test2')
-def test2():
-    url = 'http://userserve-ak.last.fm/serve/300x300/96864147.png'
-    url2 = 'http://userserve-ak.last.fm/serve/300x300/62328943.png'
-
-    response = requests.get(url)
-    response2 = requests.get(url2)
-
-    new_im = Image.new('RGB', (400,400))
-
-    img_io = StringIO(response.content)
-    img_io2 = StringIO(response2.content)
-
-    i = Image.open(img_io)
-    i2 = Image.open(img_io2)
-
-    i.thumbnail((100,100))
-    i2.thumbnail((100,100))
-
-    new_im.paste(i, (0,0))
-    new_im.paste(i2, (100,0))
-
-    out_img = StringIO()
-    new_im.save(out_img, 'PNG')
-
-    out_img.seek(0)
-
-    return send_file(out_img, mimetype='image/png')
-
 @app.route('/genimg')
 def genimg():
     width = 1366
     height = 768
-    url = 'http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=fakelbst&period=6month&api_key=4dff88a0423651b3570253b10b745b2c&format=json&limit=50&page=1'
+    url = 'http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=fakelbst&api_key=4dff88a0423651b3570253b10b745b2c&format=json&limit=50&page=1'
     response = requests.get(url)
     data =  json.loads(response.text)
     print data
@@ -70,7 +65,6 @@ def genimg():
         for j in xrange(0, height, 100):
             print i
             print j
-
             img = random.choice(img_urls)
             r = requests.get(img)
             img_io = StringIO(r.content)
@@ -85,4 +79,10 @@ def genimg():
     out_img.seek(0)
 
     return send_file(out_img, mimetype='image/png')
+
+@app.route('/test_celery')
+def test_celery():
+    result = add_together.delay(23, 42)
+    print(result.get())
+    return jsonify({})
 
